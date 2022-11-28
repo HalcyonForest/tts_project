7a0a8ff831dc8a3b6df109a44c0b663222bd3479
Egor Tkachenko <68852747+HalcyonForest@users.noreply.github.com>
2022-11-28 20:56:46 +0300

    Add files via upload

---
+++ model/encoder.py
@@ -0,0 +1,140 @@
+from torch import nn
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+# - Flash Attention - https://arxiv.org/abs/2205.14135
+
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, q, k, v, mask=None):
+        # q, k, v: [ (batch_size * n_heads) x seq_len x hidden_size ]
+        
+        QK = torch.bmm(q, torch.transpose(k, 1, 2))
+        
+        QK = QK / self.temperature
+
+        if mask is not None:
+#             print(mask)
+            QK = QK.masked_fill(mask, -np.inf)
+
+        attention = self.softmax(QK)
+        res = self.dropout(attention)
+        res = torch.bmm(attention, v)
+        
+        
+        
+        # attn: [ (batch_size * n_heads) x seq_len x seq_len ]
+
+        ### Your code here
+
+        # output: [ (batch_size * n_heads) x seq_len x hidden_size ]
+
+        return res, attention
+
+class MultiHeadAttention(nn.Module):
+    ''' Multi-Head Attention module '''
+
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+        self.d_model = d_model
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k)
+        self.w_ks = nn.Linear(d_model, n_head * d_k)
+        self.w_vs = nn.Linear(d_model, n_head * d_v)
+
+        self.attention = ScaledDotProductAttention(
+            temperature=d_k**0.5) 
+        self.layer_norm = nn.LayerNorm(d_model)
+
+        self.fc = nn.Linear(n_head * d_v, d_model)
+        nn.init.xavier_normal_(self.fc.weight)
+
+        self.dropout = nn.Dropout(dropout)
+        
+        self.reset_parameters()
+
+    def reset_parameters(self):
+         # normal distribution initialization better than kaiming(default in pytorch)
+        nn.init.normal_(self.w_qs.weight, mean=0,
+                        std=np.sqrt(2.0 / (self.d_model + self.d_k)))
+        nn.init.normal_(self.w_ks.weight, mean=0,
+                        std=np.sqrt(2.0 / (self.d_model + self.d_k)))
+        nn.init.normal_(self.w_vs.weight, mean=0,
+                        std=np.sqrt(2.0 / (self.d_model + self.d_v))) 
+        
+    def forward(self, q, k, v, mask=None):
+#         print("Encoder forward QKV: ", q, k, v)
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+
+        sz_b, len_q, _ = q.size()
+        sz_b, len_k, _ = k.size()
+        sz_b, len_v, _ = v.size()
+
+        residual = q
+
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        q = q.permute(2, 0, 1, 3).contiguous().view(-1, len_q, d_k)  # (n*b) x lq x dk
+        k = k.permute(2, 0, 1, 3).contiguous().view(-1, len_k, d_k)  # (n*b) x lk x dk
+        v = v.permute(2, 0, 1, 3).contiguous().view(-1, len_v, d_v)  # (n*b) x lv x dv
+        
+        if mask is not None:
+            mask = mask.repeat(n_head, 1, 1)  # (n*b) x .. x ..
+        output, attn = self.attention(q, k, v, mask=mask)
+
+        output = output.view(n_head, sz_b, len_q, d_v)
+        output = output.permute(1, 2, 0, 3).contiguous().view(sz_b, len_q, -1)  # b x lq x (n*dv)
+
+        output = self.dropout(self.fc(output))
+        output = self.layer_norm(output + residual)
+
+        return output, attn
+
+class PositionwiseFeedForward(nn.Module):
+    ''' A two-feed-forward-layer module '''
+
+    def __init__(self, d_in, d_hid, dropout=0.1, model_config=None):
+        super().__init__()
+
+        # Use Conv1D
+        # position-wise
+        self.w_1 = nn.Conv1d(
+            d_in, d_hid, kernel_size=model_config.fft_conv1d_kernel[0], padding=model_config.fft_conv1d_padding[0])
+        # position-wise
+        self.w_2 = nn.Conv1d(
+            d_hid, d_in, kernel_size=model_config.fft_conv1d_kernel[1], padding=model_config.fft_conv1d_padding[1])
+
+        self.layer_norm = nn.LayerNorm(d_in)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        residual = x
+        x = self.layer_norm(x)
+        output = x.transpose(1, 2)
+        output = self.w_2(F.relu(self.w_1(output)))
+        output = output.transpose(1, 2)
+        output = self.dropout(output)
+#         output = self.layer_norm(output + residual)
+        output = output + residual
+#         residual = x
+#         output = x.transpose(1, 2)
+#         output = self.w_2(F.relu(self.w_1(output)))
+#         output = output.transpose(1, 2)
+#         output = self.dropout(output)
+#         output = self.layer_norm(output + residual)
+
+        return output
